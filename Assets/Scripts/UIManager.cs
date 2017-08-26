e05f6f9795970a0a8e23c9e4d2c63f3e518b9b1d
Adrien Leravat <Pixep@users.noreply.github.com>
2017-08-26 17:50:40 +0200

    Made start/stop buttons exclusive

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace Landau {
     public class UIManager : MonoBehaviour {
         static private UIManager _instance;
+
+        private Button StartButton;
+        private Button StopButton;
 
         void Awake()
         {
@@ -23,15 +28,24 @@ namespace Landau {
         // Use this for initialization
         void Start() {
             Main.Instance().SetUIManager(this);
+
+            StartButton = GameObject.Find("StartControlButton").GetComponent<Button>();
+            Assert.IsNotNull(StartButton);
+            StopButton = GameObject.Find("StopControlButton").GetComponent<Button>();
+            Assert.IsNotNull(StopButton);
         }
 
         public void StartAutomaticControl()
         {
             Main.Instance().ControlUnit().StartUnit();
+            StartButton.interactable = false;
+            StopButton.interactable = true;
         }
         public void StopAutomaticControl()
         {
             Main.Instance().ControlUnit().StopUnit();
+            StartButton.interactable = true;
+            StopButton.interactable = false;
         }
     }
 }
