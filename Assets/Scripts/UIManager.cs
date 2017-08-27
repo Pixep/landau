dd83837b36b5237360096dd18440e0665ce4263c
Adrien Leravat <Pixep@users.noreply.github.com>
2017-08-27 17:21:39 +0200

    Fix Start button not working

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -45,7 +45,7 @@ namespace Landau {
 
         public void ToggleControl()
         {
-            if (Main.Instance().ControlUnit()._running)
+            if (!Main.Instance().ControlUnit()._running)
                 Main.Instance().ControlUnit().StartUnit();
             else
                 Main.Instance().ControlUnit().StopUnit();
@@ -57,37 +57,26 @@ namespace Landau {
         {
             ProtocolState state = Main.Instance().ControlUnit().Protocol.State;
             String text = "ControlUnit: ";
+
             if (state != ProtocolState.ConnectedState)
-            {
                 text += "Not connected";
-            }
             else
-            {
                 text += "Connected";
-            }
 
             state = Main.Instance().SensorsMgr().Protocol.State;
             text += "\nSensorsMgr: ";
+
             if (state != ProtocolState.ConnectedState)
-            {
                 text += "Not connected";
-            }
             else
-            {
                 text += "Connected";
-            }
+
             _statusText.text = text;
 
             if (Main.Instance().ControlUnit()._running)
-            {
                 _startButton.GetComponentInChildren<Text>().text = "Stop";
-                _startButton.interactable = false;
-            }
             else
-            {
                 _startButton.GetComponentInChildren<Text>().text = "Start";
-                _startButton.interactable = true;
-            }
         }
 
         private void StateChanged(object sender, EventArgs e)
